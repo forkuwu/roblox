e5b8f0b79c8355ecc10802e6b441b029e15f3997
xuvhh <92933676+forkuwu@users.noreply.github.com>
2023-01-11 07:26:57 -0500

    Update and rename roblox.html to roblox.js

---
+++ roblox.js
@@ -0,0 +1,3 @@
+document.write("<div style=\"padding-top:5px;border-radius: 25px 25px 25px 25px;\" class=\"card animals\" loading=\"lazy\"><a onclick=\"go('https://kxd.fm/roblox/v2')\">");
+document.write("    <img style=\"border-radius: 25px;\" src=\"roblox.png\"></a>");
+document.write("    <p3>Roblox</p3>");
